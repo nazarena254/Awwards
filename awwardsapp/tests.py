3d422a92e5f46fcf5727e6d4392a88aa58d6c209
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-12 23:00:45 +0300

    add tests for my Profile, Project and Rate models

--- awwardsapp/tests.py
+++ awwardsapp/tests.py
@@ -1,3 +1,76 @@
 from django.test import TestCase
+from .models import *
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user=User(username='alvynah')
+        self.user.save()
+        self.profile=Profile(user=self.user,name='vee',bio='vee in the house',prof_pic='default.png',location='kenya',account_url='www.alvynah.com')
+    def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+    def test_instance(self):
+         self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_saveProfile(self):
+        self.profile.save_profile()
+        profile_saved = Profile.objects.all()
+        self.assertTrue(len(profile_saved) > 0)
+    def test_delete_method(self):
+
+
+        self.user.delete()
+        profile = User.objects.all()
+        self.assertTrue(len(profile) == 0)
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.user=User(username='alvynah')
+        self.user.save()
+        self.project=Project(title='Test title',description='test description', project_image='test.png', project_url='project.com',technologies="php",user=self.user)
+    def tearDown(self):
+        Project.objects.all().delete()
+        User.objects.all().delete()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+    def test_save_Project(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)>0)
+
+    def test_delete_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)==0)
+
+class RateTest(TestCase):
+    def setUp(self):
+        self.user=User(username='alvynah')
+        self.user.save()
+        self.project=Project(title='Test title',description='test description', project_image='test.png', project_url='project.com',technologies="php",user=self.user)
+        self.project.save()
+        self.rate=Rate(design='9',usability='10',content='9',user=self.user,project=self.project)
+        self.rate.save_rating() 
+    def tearDown(self):
+        Rate.objects.all().delete()
+        Project.objects.all().delete()
+        User.objects.all().delete()
+    def test_instance(self):
+        self.assertTrue(isinstance(self.rate,Rate)) 
+    def test_save_rates(self):
+        self.rate.save_rating()
+        rates=Rate.objects.all()
+        self.assertTrue(len(rates)==1)
+    def test_delete_rates(self):
+        self.rate.save_rating()
+        self.rate.delete_rating()
+        rates = Rate.objects.all()
+        self.assertTrue(len(rates)==0)
+
+
+
--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,116 +1,123 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.models import User
-from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest,Http404,HttpResponse,HttpResponseRedirect
-from .models import Profile, Project, Rate
-from .email import send_welcome_email
 from .forms import *
+from .models import *
+from .email import send_welcome_email
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def signup_view(request):
-    if request.method=="POST":
-        form=SignUpForm(request.POST)
-        if form.is_valid:
+   if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+
             form.save()
-            #cleaned_data.get()-Any data user submits thru form will be passed to server as strings
-            username=form.cleaned_data.get("username")
-            password=form.cleaned_data.get("password1")
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
             name=form.cleaned_data['username']
             email=form.cleaned_data['email']
-            send_welcome_email(name, email)
-            user=authenticate(username=username, password=password)
-            login(request,user)
+            
+            send_welcome_email(name,email)
+            user = authenticate(username=username, password=password)
+
+            login(request, user)
+
+
             return redirect('welcome')
-        else:
-            form=SignUpForm()
-        return render(request, 'registration/signup', {"form":form})     
+   else:
+        form = SignUpForm()
+   return render(request, 'registration/signup.html', {'form': form})
+    
 
 def welcome(request):
-    users=User.object.exclude(id=request.user.id)
-    profiles=Profile.objects.all()
-    projects=Project.objects.all()
-    project_average=Rate.objects.order_by('-scores').first()
-    ratings=Rate.objects.all()
+   users = User.objects.exclude(id=request.user.id)
+   profiles=Profile.objects.all()
+   projects=Project.objects.all()
+   project_average=Rate.objects.order_by('-score').first()
+   ratings=Rate.objects.all()
+   
+ 
 
-    params={
-        'users':users,
-        'profiles':profiles,
-        'projects':projects,
-        'project_average':project_average,
-        'ratings':ratings,
+          
+   params={
+       'users':users,
+       'profiles':profiles,
+       'projects':projects,
+       'project_average':project_average,
+       'ratings':ratings,
     }
-    return render(request,'awwards/index.html',params)
+   return render(request,'awwards/index.html',params)
 
 @login_required
 def upload_project(request):
-    if request.method=="POST":
-        form=UploadProjectForm(request.POST, request.FILES)
-        if form.is_valid():
+      if request.method=='POST':
+         form=UploadProjectForm(request.POST, request.FILES)
+         if form.is_valid():
             project=form.save(commit=False)
             project.user=request.user
             project.save()
             return redirect('welcome')
-        else:
-            form=UploadProjectForm()
-        return render(request, 'awwards/project.html',{"form":form}) 
-
+      else:
+         form=UploadProjectForm()
+      return render(request,'awwards/project.html',{'form':form})
+ 
 @login_required
-def rate_project(request, project_title):
-    project=Project.objects.get(title=project_title) 
-    rates=Rate.objects.filter(user=request.user,project=project).first()
-    ratings=Rate.objects.order_by('-rated_at')
-    rates_status=None
-    if rates is None:
-        rate_status=False
-    else:
-        rate_status=True
+def rate_project(request,project_title):
+   project=Project.objects.get(title=project_title)
+   rates=Rate.objects.filter(user=request.user,project=project).first()
+   ratings=Rate.objects.order_by('-rated_at')
+   rates_status=None
+   if rates is None:
+          rates_status = False
+   else:
+          rates_status = True
 
-    if request.method=='POST':
-        form=RateForm(request.POST)
-        if form.is_valid():
-            rate=form.save(commit=False)
-            rate.user=request.user
-            rate.project=project
-            rate.save()
-            project_ratings=Rate.objects.filter(project=project)
+   if request.method == 'POST':
+         form = RateForm(request.POST)
+         if form.is_valid():
+               rate =form.save(commit=False)
+               rate.user=request.user
+               rate.project=project
+               rate.save()
+               project_ratings=Rate.objects.filter(project=project)
 
-            design = form.cleaned_data['design']
-            usability = form.cleaned_data['usability']
-            content = form.cleaned_data['content']
-            rate.design = design
-            rate.usability = usability
-            rate.content = content
-            rate.average = (rate.design + rate.usability + rate.content)/3
-            rate.save()
+               design = form.cleaned_data['design']
+               usability = form.cleaned_data['usability']
+               content = form.cleaned_data['content']
+               rate.design = design
+               rate.usability = usability
+               rate.content = content
+               rate.average = (rate.design + rate.usability + rate.content)/3
+               rate.save()
 
-            design_ratings = [d.design for d in project_ratings]
-            design_average = sum(design_ratings) / len(design_ratings)
+               design_ratings = [d.design for d in project_ratings]
+               design_average = sum(design_ratings) / len(design_ratings)
 
-            usability_ratings = [us.usability for us in project_ratings]
-            usability_average = sum(usability_ratings) / len(usability_ratings)
+               usability_ratings = [us.usability for us in project_ratings]
+               usability_average = sum(usability_ratings) / len(usability_ratings)
 
-            content_ratings = [content.content for content in project_ratings]
-            content_average = sum(content_ratings) / len(content_ratings)
-            score = (design_average + usability_average + content_average) / 3
+               content_ratings = [content.content for content in project_ratings]
+               content_average = sum(content_ratings) / len(content_ratings)
+               score = (design_average + usability_average + content_average) / 3
 
-            rate.design_average = round(design_average, 2)
-            rate.usability_average = round(usability_average, 2)
-            rate.content_average = round(content_average, 2)
-            rate.score = round(score, 2)
+               rate.design_average = round(design_average, 2)
+               rate.usability_average = round(usability_average, 2)
+               rate.content_average = round(content_average, 2)
+               rate.score = round(score, 2)
 
-            rate.save()
-            return HttpResponseRedirect(request.path_info)    
-    else:
-        form=RateForm()
-
-    params ={
+               rate.save()
+               return HttpResponseRedirect(request.path_info)
+   else:
+          form=RateForm()
+   params ={
       'project':project,
       'rates_status':rates_status,
       'rating_form':form,
       'ratings':ratings,
-     }
-    return render(request,'awwards/voteproject.html',params)
+   }
+   return render(request,'awwards/voteproject.html',params)
 
 @login_required
 def search_project(request):
@@ -185,7 +192,7 @@ def profile(request, username):
 
     }
    return render(request, 'awwards/profile.html', params)
-
+   
 def user_profile(request, username):
    current_user = request.user
    user_profile = get_object_or_404(User, username=username)
@@ -200,7 +207,3 @@ def user_profile(request, username):
 
     }
    return render(request, 'awwards/user_profile.html', params)
-
-
-
-
