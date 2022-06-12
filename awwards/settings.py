320ae4918fd8a93760f051bc27c1db7bf9aadf05
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-12 20:38:09 +0300

    add profile model

--- awwards/settings.py
+++ awwards/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'awwards.wsgi.application'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=config("DEBUG")
+DEBUG=config("DEBUG", default=False, cast=bool)
 MODE=config("MODE", default="dev")
 ALLOWED_HOSTS='nazawwards.herokuapp.com'
 
--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    prof_pic=models.ImageField(upload_to='pictures/',default='default.png')
+    name=models.CharField(max_length=50)
+    bio=models.TextField()
+    location=models.CharField(max_length=100)
+    account_url=models.URLField()
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, search_term):
+        return cls.objects.filter(user__username__icontains=search_term).all()
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
