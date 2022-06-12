b9fea0327371adefd46bae3f88ede023743e42c2
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-06-12 21:12:50 +0300

    install cloudinary and add all its configurations

--- awwards/settings.py
+++ awwards/settings.py
@@ -152,6 +152,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'welcome'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
